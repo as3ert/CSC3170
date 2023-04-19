412a09e68c94bc0da584b7e6cb9777f5e4d7f8d0
as3ert <as3ertpro@gmail.com>
2023-04-19 23:41:47 +0800

    Update front

--- front/work/worker.php
+++ front/work/worker.php
@@ -51,7 +51,7 @@ foreach($rs as $k => $item) {
 			<!-- The Grid -->
 			<div class="w3-row">
 				<!-- Left Column -->
-				<div class="w3-col m3">
+				<div class="w3-col m2">
 					<div class="w3-card w3-round w3-white">
 						<div class="w3-container">
 							<h4 class="w3-center">My Profile</h4>
@@ -70,11 +70,11 @@ foreach($rs as $k => $item) {
 				<!-- End Left Column -->
 				</div>
 				<!-- Right Column -->
-				<div class=" main w3-col m7">
+				<div class=" main w3-col m10">
 				<?php
 				foreach($projectList as $k => $project){
 					echo "<section class='w3-container w3-card w3-white w3-round w3-margin' id='prj1'><br>";
-					echo "<h2 class='w3-text-grey w3-padding-16'><i class='fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-theme'></i>Project 1 (name):</h2>";
+					echo "<h2 class='w3-text-grey w3-padding-10'><i class='fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-theme'></i>Project 1 (name):</h2>";
 					echo "<div class='w3-container'>";
 					echo "<h5 class='w3-opacity'><b></b></h5>";
 					echo "<h6 class='w3-text-theme'><i class='fa fa-align-right fa-fw w3-margin-right'></i><span class='w3-tag w3-theme w3-round'>{$project['PROJECT_ID']}</span></h6>";
